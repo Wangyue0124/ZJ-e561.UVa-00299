56407534e3b102eeabcce8fa07a38fa34f33d665
Wangyue0124 <diego960124@gmail.com>
2025-09-28 18:39:57 +0800

    Add files via upload

---
+++ e561. 00299 - Train Swapping.cpp
@@ -0,0 +1,26 @@
+﻿#include <iostream>
+#include <vector>
+using namespace std;
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(nullptr);
+	int n;
+	cin >> n;
+	while (n--) {
+		int l, s = 0;
+		cin >> l;
+		vector<int>train(l);
+		for (int i = 0; i < l; i++) cin >> train[i];
+		for (int i = l-1; i > 0; i--) {
+			for (int a = 0, b = 1; b <= i; a++, b++) {
+				if (train[a] > train[b]) {
+					swap(train[a], train[b]);
+					s++;
+				}
+			}
+		}
+		cout << "Optimal train swapping takes " << s << " swaps.\n";
+	}
+	return 0;
+}
+
